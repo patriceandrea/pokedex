713e4129ff0ff75673a559e3e9cb746de77d28d8
patriceandrea <andrea_pavico98@yahoo.ca>
2022-10-04 12:55:07 -0400

    fixes the images

--- client/src/Pokemon.js
+++ client/src/Pokemon.js
@@ -1,26 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { Button, CircularProgress, Typography } from '@material-ui/core';
 import { toFirstCharUppercase } from "./constants"
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Pokemon = ({ match, history }) => {
   const { params } = match;
+  const { pokemonId } = params;
   const [pokemon, setPokemon] = useState(undefined);
 
 
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+      .then(function (response) {
+        const { data } = response;
+        setPokemon(data);
+      }
+
+      )
+  })
+
   const generatePokemonJSX = () => {
     const { name, id, species, height, weight, types, sprites } = pokemon;
-    const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
-    const { front_default } = sprites;
+    const { front_default, front_shiny } = sprites;
 
     return (
       <>
         <Typography variant="h1">
           {`${id}.`} {toFirstCharUppercase(name)}
-          <img src={front_default} />
+          <img src={front_shiny} />
         </Typography>
-        <img style={{ width: "300px", height: "300px" }} src={fullImageUrl} />
+        <img style={{ width: "300px", height: "300px" }} src={front_default} />
         <Typography variant="h3">Pokemon Info</Typography>
         <Typography>
           {"Species: "}
